6c058a7c95b5e681a174cff0f7d6d26f7f4d8383
CrisDesa <crispedrani@gmail.com>
2018-07-21 22:37:48 -0300

    Creacion de aplicacion WEB MVC a partir de template Visual Studio Net. core

---
+++ iCiS/iCiS/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using iCiS.Models;
+
+namespace iCiS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "iCiS Información Centralizada Infraestructura Sistemas";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Felino Software";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ iCiS/iCiS/Controllers/iCiSController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace iCiS.Controllers
+{
+    public class iCiSController : Controller
+    {
+        // GET: /iCiS/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /iCiS/bienvenido/
+        public IActionResult Bienvenido(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
